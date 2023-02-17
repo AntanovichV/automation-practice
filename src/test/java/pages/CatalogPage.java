b7da9ae083298524f1fe585f4ff77867e2c2a985
Valentina Antanovich <v.antanovich@godeltech.com>
2023-02-17 09:34:55 +0100

    fix: correct condition

--- src/test/java/pages/CatalogPage.java
+++ src/test/java/pages/CatalogPage.java
@@ -3,20 +3,21 @@ package pages;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
+
 import static com.codeborne.selenide.Selenide.$;
 
 public class CatalogPage extends BasePage {
-    private SelenideElement jacketStyleLabel = $(By.xpath("//*[@id=\"narrow-by-list\"]/div[1]/div[1]"));
-    private SelenideElement rainCoatStyleLabel = $(By.xpath("//*[@id=\"narrow-by-list\"]/div[1]/div[2]/ol/li[6]/a"));
-    private SelenideElement listViewButton = $(By.xpath("//*[@id=\"mode-list\"]"));
-    private SelenideElement sortByPriceLabel = $(By.xpath("//*[@id=\"sorter\"]/option[3]"));
-    private SelenideElement firstItemSizeButton = $(By.xpath("//*[@id=\"option-label-size-143-item-168\"]"));
-    private SelenideElement firstItemColorButton = $(By.xpath("//*[@id=\"option-label-color-93-item-58\"]"));
-    private SelenideElement addToCartButton = $(By.xpath("//*[@title=\"Add to Cart\"]"));
-    private SelenideElement productAddedMessageLabel = $(By.xpath("//*[@id=\"maincontent\"]/div[2]/div[2]/div/div/div"));
-    private SelenideElement productCartNumberLabel = $(By.xpath("//span[@class='counter qty']"));
-    private SelenideElement cartLabel = $(By.xpath("//a[@class='action showcart']"));
-    private SelenideElement editCartLabel = $(By.xpath("//a[@class='action viewcart']"));
+    private final SelenideElement jacketStyleLabel = $(By.xpath("//*[@id=\"narrow-by-list\"]/div[1]/div[1]"));
+    private final SelenideElement rainCoatStyleLabel = $(By.xpath("//*[@id=\"narrow-by-list\"]/div[1]/div[2]/ol/li[6]/a"));
+    private final SelenideElement listViewButton = $(By.xpath("//*[@id=\"mode-list\"]"));
+    private final SelenideElement sortByPriceLabel = $(By.xpath("//*[@id=\"sorter\"]/option[3]"));
+    private final SelenideElement firstItemSizeButton = $(By.xpath("//*[@id=\"option-label-size-143-item-168\"]"));
+    private final SelenideElement firstItemColorButton = $(By.xpath("//*[@id=\"option-label-color-93-item-58\"]"));
+    private final SelenideElement addToCartButton = $(By.xpath("//*[@title=\"Add to Cart\"]"));
+    private final SelenideElement productAddedMessageLabel = $(By.xpath("//*[@id=\"maincontent\"]/div[2]/div[2]/div/div/div"));
+    private final SelenideElement productCartNumberLabel = $(By.xpath("//span[@class='counter qty']"));
+    private final SelenideElement cartLabel = $(By.xpath("//a[@class='action showcart']"));
+    private final SelenideElement editCartLabel = $(By.xpath("//a[@class='action viewcart']"));
 
     public CatalogPage() {
         super("//header//img", "Catalog page");
@@ -56,11 +57,12 @@ public class CatalogPage extends BasePage {
     }
 
     public String getProductAddedMessage() {
+        productAddedMessageLabel.shouldBe(Condition.appear);
         return productAddedMessageLabel.getText();
     }
 
     public void clickCart() {
-        productCartNumberLabel.shouldBe(Condition.visible);
+        productCartNumberLabel.shouldBe(Condition.appear);
         cartLabel.click();
         editCartLabel.click();
     }
